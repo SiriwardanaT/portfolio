c24c3778f9c2c6054ece301ccce3fa67726a3021
SiriwardanaT <thariharshana123@gmail.com>
2021-05-26 17:47:40 +0530

    change navi bar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Nav from './components/nav'
+import Header from './components/header'
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/header.css'
+function  header() {
+    return (
+        <div>
+            <div className="header_container">
+                <h1>Hello , I Am Developer</h1>
+            </div>
+        </div>
+    )
+}
+
+export default header
